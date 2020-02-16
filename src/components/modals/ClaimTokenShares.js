1c0002b9ecfa77e691a3a3956fbe362adc444b00
Aaron Anderson <aaronlynanderson@gmail.com>
2020-02-16 03:08:54 -0700

    claim token functions

--- src/components/modals/ClaimTokenShares.js
+++ src/components/modals/ClaimTokenShares.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import { claimTokenShares } from '../../utils/claimTokenShares'
+import { getTrackedTokens } from '../../utils/getTrackedTokens'
 
-const useStyles = makeStyles(theme => ({
+const styles = {
   root: {
-    '& > *': {
       position: "absolute",
       margin: "5vw",
       left: "0px",
@@ -13,31 +14,48 @@ const useStyles = makeStyles(theme => ({
       height: "70vh",
       display: "flex",
       flexDirection: "column",
-    },
   },
   input: {
     border: "1px solid grey",
     backgroundColor: "white",
   }
-}));
+};
 
-export default function ClaimTokenShares(props) {
-  const classes = useStyles();
+export default class ClaimTokenShares extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      tokenAddresses: null,
+    }
+  }
 
-  return (
-    <div className={classes.root}>
-      <Paper elevation={3}>
-        <label>
-          Available tokens you can claim
-        </label>
+  componentDidMount(){
+    // get trackedTokens
+    getTrackedTokens().then((res)=>{
+      console.log("trackedTokens:", res)
+      this.setState({tokenAddresses: res})
+    }).catch(alert("No tokens have been registered"))
+  }
 
-        <button onClick={()=>{
-          // call AddBeneficiary on contract, notify of success, then hide modal
-          props.hide()
-        }}>
-          Claim your inheritance
-        </button>
-      </Paper>
-    </div>
-  );
+  render(){
+    return (
+      <div>
+        <Paper elevation={3} style={styles.root}>
+          <label>
+            Available tokens you can claim
+          </label>
+          <select>
+          {this.state.tokenAddress && this.state.tokenAddresses.map(e=> <option value={e}>e</option>)}
+          </select>
+
+          <button onClick={()=>{
+            // call AddBeneficiary on contract, notify of success, then hide modal
+            this.props.hide()
+          }}>
+            Claim your inheritance
+          </button>
+        </Paper>
+      </div>
+    );
+  }
 }
